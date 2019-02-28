bc0fa6f04e826c35923a344e3b34af87d4e984ca
d.scalera <d.scalera@BLU19779.replynet.prv>
2019-02-28 20:31:22 +0100

    commit umbe dome

--- ChallengeProject/src/it/hashcode/sevincomilicenzio/fanculo/Util.java
+++ ChallengeProject/src/it/hashcode/sevincomilicenzio/fanculo/Util.java
@@ -1,5 +1,8 @@
 package it.hashcode.sevincomilicenzio.fanculo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Util {
@@ -9,14 +12,48 @@ public class Util {
 		return null;
 	}
 
-	public static List<Photo> preprocessPhase(List<Photo> photos) {
-		// TODO Auto-generated method stub
-		return null;
+	public static List<Slide> preprocessPhase(List<Photo> photos) {
+		long start = System.currentTimeMillis();
+		Collections.shuffle(photos);
+		
+		List<Slide> returnList = new ArrayList<Slide>();
+
+		Photo tmpPhoto1 = new Photo();
+		Photo tmpPhoto2 = new Photo();
+		boolean isFirstPhoto = true;
+		for(Photo p : photos) {
+			
+			if(p.getOrientamento() == 1) {
+				if(isFirstPhoto) {
+					tmpPhoto1 = p;
+					isFirstPhoto = false;
+				} else {
+					tmpPhoto2 = p;
+					Slide tmpSlide = new Slide(tmpPhoto1, tmpPhoto2);
+					returnList.add(tmpSlide);
+					tmpPhoto1 = new Photo();
+					tmpPhoto2 = new Photo();
+					isFirstPhoto = true;
+				}
+			} else {
+				returnList.add(new Slide(p, null));
+			}
+		}
+		long finish = System.currentTimeMillis();
+		long timeElapsed = finish - start;
+		return returnList;
 	}
 
-	public static List<Slide> calulatingPhase(List<Photo> photos) {
-		// TODO Auto-generated method stub
-		return null;
+	public static List<Slide> calulatingPhase(List<Slide> slides) {
+		long start = System.currentTimeMillis();
+		List<Slide> returnList =  new ArrayList<Slide>();
+		
+		returnList.sort(c);
+		
+		
+		long finish = System.currentTimeMillis();
+		long timeElapsed = finish - start;
+		return returnList;
 	}
 
 	public static void writeFile(List<Slide> slides, String outputpath) {
